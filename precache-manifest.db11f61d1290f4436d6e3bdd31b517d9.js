d825a346820e231f4383b35e3084bd63577771ab
Jakub Strebeyko <strebeyko@gmx.com>
2019-03-31 23:32:28 +0200

    Updates

---
+++ precache-manifest.db11f61d1290f4436d6e3bdd31b517d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/typer/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bf493d931a21f6f6fd71",
+    "url": "/typer/static/js/runtime~main.24816a52.js"
+  },
+  {
+    "revision": "a2fe9dd2327bd75c4a96",
+    "url": "/typer/static/js/main.90fc25c6.chunk.js"
+  },
+  {
+    "revision": "25489ecae4f8970e8158",
+    "url": "/typer/static/js/2.6b556fef.chunk.js"
+  },
+  {
+    "revision": "a2fe9dd2327bd75c4a96",
+    "url": "/typer/static/css/main.801b7620.chunk.css"
+  },
+  {
+    "revision": "9e338cba0b59b31b6d0136fcaef0c605",
+    "url": "/typer/index.html"
+  }
+];
